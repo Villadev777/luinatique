a95f435f9b81295e94d2aaf91cfdc3ab8efa6b31
Villadev777 <giustyvillanueva@gmail.com>
2025-09-09 12:08:02 -0500

    Fix Vite React SWC plugin error

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,10 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    react(),
+    react({
+      jsxImportSource: 'react',
+      tsDecorators: true
+    }),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
