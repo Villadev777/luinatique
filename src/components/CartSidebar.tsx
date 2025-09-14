7b8e7364373b2daa5c2853cc6303288b4ea6566e
Villadev777 <giustyvillanueva@gmail.com>
2025-09-14 16:58:34 -0500

    Updated CartSidebar.tsx

--- src/components/CartSidebar.tsx
+++ src/components/CartSidebar.tsx
@@ -27,7 +27,7 @@ export const CartSidebar: React.FC<CartSidebarProps> = ({ children }) => {
   const [discount, setDiscount] = React.useState(0);
 
   const formatPrice = (price: number) => {
-    return new Intl.NumberFormat('es-PE', {
+    return new Intl.NumberFormat('es-MX', {
       style: 'currency',
       currency: 'PEN',
     }).format(price);
