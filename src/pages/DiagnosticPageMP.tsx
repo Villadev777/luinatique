8344a9a235c4f8f8ddb6d2b1790aaf9935d3d3f5
Villadev777 <giustyvillanueva@gmail.com>
2025-09-17 22:14:13 -0500

    feat: Add diagnostic page for MercadoPago integration testing

---
+++ src/pages/DiagnosticPageMP.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MercadoPagoDiagnostic from '../components/MercadoPagoDiagnostic';
+
+const DiagnosticPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-background py-8">
+      <MercadoPagoDiagnostic />
+    </div>
+  );
+};
+
+export default DiagnosticPage;
