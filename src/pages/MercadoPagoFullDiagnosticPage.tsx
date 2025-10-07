7ba06196363e4309b16224471155cb8e4e6b0b20
Villadev777 <giustyvillanueva@gmail.com>
2025-10-07 18:57:07 -0500

    feat: Add MercadoPago Full Diagnostic page

---
+++ src/pages/MercadoPagoFullDiagnosticPage.tsx
@@ -0,0 +1,7 @@
+import MercadoPagoFullDiagnostic from '../components/MercadoPagoFullDiagnostic';
+
+const MercadoPagoFullDiagnosticPage = () => {
+  return <MercadoPagoFullDiagnostic />;
+};
+
+export default MercadoPagoFullDiagnosticPage;
